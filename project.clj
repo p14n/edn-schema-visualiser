f6ae6c013db2ae908630b538c90821f9e63646a6
Dean Chapman <dean@p14n.com>
2017-05-05 14:16:48 +0100

    Generate a working site again

--- project.clj
+++ project.clj
@@ -2,23 +2,22 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2311"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.521"]
                  [om "0.7.1"]
                  [sablono "0.2.20"]
                  [testdouble/clojurescript.csv "0.1.1"]
                  [com.cognitect/transit-cljs "0.8.184"]
                  [inflections "0.9.9"]]
 
-  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
-
-  :source-paths ["src"]
+  :plugins [[lein-cljsbuild "1.1.6"]]
+  ;:source-paths ["src"]
 
   :cljsbuild {
-    :builds [{:id "csv-visualiser"
+    :builds [{;:id "csv-visualiser"
               :source-paths ["src"]
               :compiler {
-                :output-to "csv_visualiser.js"
-                :output-dir "out"
+                :output-to "public/out/main/csv_visualiser.js"
+                :output-dir "public/out"
                 :optimizations :none
                 :source-map true}}]})
